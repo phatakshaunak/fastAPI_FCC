4c8e4c2b46751f16cb0d9cc80e8cafd95899210a
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2022-03-13 21:41:27 +0530

    using pydantic to validate schema of received data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from fastapi import Body, FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,8 +14,21 @@ def root():
 def get_posts():
     return {'data': 'some placeholder post'}
 
+# Schema that front end should ensure being sent
+class Post(BaseModel):
+    title: str
+    content: str
+    # optional as default value is set
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.post("/createposts")
-def create_posts(payload = Body(...)):
-    print(payload)
-    return {"new post": f"title: {payload['title']} content: {payload['content']}"}
+def create_posts(post: Post):
+    to_return  = post.dict()
+    print(post, type(post))
+    print(to_return ,type(to_return))
+
+    return to_return
+    # print(payload)
+    # return {"new post": f"title: {payload['title']} content: {payload['content']}"}
